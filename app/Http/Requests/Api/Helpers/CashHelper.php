47137469e337f1fb81d51123167ac07af6dfed66
Darío Rivera <fermius.us@gmail.com>
2021-03-27 23:54:03 -0500

    refactor: improve casting

--- app/Http/Requests/Api/Helpers/CashHelper.php
+++ app/Http/Requests/Api/Helpers/CashHelper.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Api\Helpers;
 
 class CashHelper
 {
-    public static function sum(array $cash)
+    public static function sum(array $cash): int
     {
         return array_sum(array_map(function ($entry) {
             return $entry['amount'] * $entry['quantity'];
