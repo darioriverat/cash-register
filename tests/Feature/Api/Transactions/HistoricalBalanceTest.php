4927afe3c4e465c3dab9afa3cc677fb8b125333e
Darío Rivera <fermius.us@gmail.com>
2021-03-29 14:06:27 -0500

    test: add test with specific datetime

--- tests/Feature/Api/Transactions/HistoricalBalanceTest.php
+++ tests/Feature/Api/Transactions/HistoricalBalanceTest.php
@@ -75,4 +75,58 @@ class HistoricalBalanceTest extends TestCase
             ]
         ]);
     }
+
+    /**
+     * @test
+     */
+    public function itCanQueryHistoricalBalanceInSpecificTime()
+    {
+        $this->loadBalance();
+
+        $machineId = Machine::select('id')->firstWhere('name', 'POS-45')->id;
+        $transaction = Transaction::factory()->create([
+            'machine_id' => $machineId,
+            'total' => 150000
+        ]);
+        TransactionDetails::factory()->create([
+            'transaction_id' => $transaction->id,
+            'exchange_type' => ExchangeType::BILL,
+            'amount' => 50000,
+            'quantity' => 3
+        ]);
+
+        // this transactions is not counted
+        $transaction = Transaction::factory()->create([
+            'machine_id' => $machineId,
+            'total' => 150000
+        ]);
+        $transaction->created_at = now()->addDay();
+        $transaction->save();
+        TransactionDetails::factory()->create([
+            'transaction_id' => $transaction->id,
+            'exchange_type' => ExchangeType::BILL,
+            'amount' => 50000,
+            'quantity' => 400
+        ]);
+
+        $response = $this
+            ->actingAs($this->user)
+            ->get(route('v1.historical-balance', ['POS-45', 'to' => now()->toIso8601String()]));
+
+        $response->assertOk();
+        $response->assertJson([
+            'status' => [
+                'code' => StatusCodes::SUCCESSFUL
+            ],
+            'resume' => [
+                'transactions' => [
+                    'base' => 105000,
+                    'income' => 150000,
+                    'outcome' => 0,
+                    'withdraw' => 0
+                ],
+                'balance' => 255000
+            ]
+        ]);
+    }
 }
